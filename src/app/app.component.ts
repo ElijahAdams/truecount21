c079aea07bed84409f491071f2c22a5d1defa4bf
Elijah <emadams92@gmail.com>
2021-01-12 22:42:56 -0500

    adding some notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'truecount21';
+/* Black jack dealing order
+  1. deal 1 card to players first face up.
+  2. deal 1 card to dealer face down
+  3. deal 2nd card to players face up
+  4. deal 2nd card to dealer face up
+ */
+
+  populateDeck() {
+    // build array with 6 decks of each card
+  }
+
+  randomCard() {
+    // card a random number between the indicies of available card. 0 through 311 -1 for each card removed.
+  }
 }
